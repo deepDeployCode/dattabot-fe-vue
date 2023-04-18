4800f21d77c65668e5e6e2de3e4a5096b48d6648
samsul <muhdevapp@gmail.com>
2023-04-18 09:01:18 +0800

    feat invoice verif: bug fix upload bukti bayar

--- src/@core/mixins/errorHandler.js
+++ src/@core/mixins/errorHandler.js
@@ -1,45 +1,45 @@
-import vue from 'vue'
-import ToastificationContent from '@core/components/toastification/ToastificationContent.vue'
+import vue from "vue";
+import ToastificationContent from "@core/components/toastification/ToastificationContent.vue";
 
 vue.mixin({
   methods: {
-    errorHandler(error, defaultMsg = 'Gagal, Silahkan coba lagi nanti') {
-      const { data } = error.response
+    errorHandler(error, defaultMsg = "Gagal, Silahkan coba lagi nanti") {
+      const { data } = error.response;
       if (error.response.status === 422 && data?.errors?.length) {
-        const self = this
+        const self = this;
         data.errors.forEach((err, i) => {
           setTimeout(() => {
             self.$toast({
               component: ToastificationContent,
               props: {
                 title: err.message,
-                icon: 'AlertCircleIcon',
-                variant: 'danger',
-                setTimeout: '5000',
+                icon: "AlertCircleIcon",
+                variant: "danger",
+                setTimeout: "5000",
               },
-            })
-          }, 3000 * i)
-        })
+            });
+          }, 3000 * i);
+        });
       } else {
         this.$toast({
           component: ToastificationContent,
           props: {
             title: data?.message || defaultMsg,
-            icon: 'AlertCircleIcon',
-            variant: 'danger',
+            icon: "AlertCircleIcon",
+            variant: "danger",
           },
-        })
+        });
       }
     },
-    successHandler(title = 'data berhasil') {
+    successHandler(title = "data berhasil") {
       this.$toast({
         component: ToastificationContent,
-        props:{
+        props: {
           title,
-          icon: 'CheckIcon',
-          variant: 'success',
-        }
-      })
-    }
+          icon: "CheckIcon",
+          variant: "success",
+        },
+      });
+    },
   },
-})
+});
--- src/views/VerifyInvoiceCalonAnggota.vue
+++ src/views/VerifyInvoiceCalonAnggota.vue
@@ -3,15 +3,16 @@
     <BaseNavigation />
     <DividerNavigation />
     <div class="p-2 mx-auto">
-      <validation-observer v-if="invoices.data && !invoices.isLoading" ref="buktiBayarValidation">
+      <validation-observer
+        v-if="invoices.data && !invoices.isLoading"
+        ref="buktiBayarValidation">
         <b-form class="mt-1" @submit.prevent>
-
           <b-card class="shadow-none border p-1 mb-1" no-body>
             <div class="d-flex pb-1 border-bottom">
               <div>
                 <div class="font-weight-bold">
                   Invoice: # {{ invoices.data.invoice.id }}
-                  <br>
+                  <br />
                   IdRekomendasi: # {{ invoices.data.user.id }}
                 </div>
                 <small>
@@ -30,25 +31,39 @@
               <div class="border-1">
                 <span>{{ invoices.data.invoice.reginvoice_status }}</span>
               </div>
-              <br>
+              <br />
               <!-- card 1 -->
               <b-card
-                v-if="invoices.data.invoice.reginvoice_status == 'sudah-dibayar' || invoices.data.invoice.reginvoice_status == 'sudah-bayar'"
-                img-alt="Card image cap" img-top no-body>
+                v-if="
+                  invoices.data.invoice.reginvoice_status == 'sudah-dibayar' ||
+                  invoices.data.invoice.reginvoice_status == 'sudah-bayar'
+                "
+                img-alt="Card image cap"
+                img-top
+                no-body>
                 <b-card-body>
                   <b-card-text>
-                    selanjutnya lakukan pengaktifan akun simfoni anda dengan cara menghubungi pihak admin.
+                    selanjutnya lakukan pengaktifan akun simfoni anda dengan
+                    cara menghubungi pihak admin.
                   </b-card-text>
                 </b-card-body>
                 <b-card-footer>
-                  <small class="text-muted"><i>Noted: jika akun anda sudah aktif maka anda dapat login di aplikasi
-                      simfoni</i></small>
+                  <small class="text-muted"
+                    ><i
+                      >Noted: jika akun anda sudah aktif maka anda dapat login
+                      di aplikasi simfoni</i
+                    ></small
+                  >
                 </b-card-footer>
               </b-card>
               <b-card v-else img-alt="Card image cap" img-top no-body>
                 <b-card-footer>
-                  <small class="text-muted"><i>Noted: harap hubungi pihak admin agar mempercepat proses aktivasi akun
-                      anda</i></small>
+                  <small class="text-muted"
+                    ><i
+                      >Noted: harap hubungi pihak admin agar mempercepat proses
+                      aktivasi akun anda</i
+                    ></small
+                  >
                 </b-card-footer>
               </b-card>
             </div>
@@ -56,30 +71,22 @@
           <b-card class="shadow-none border p-1 mb-1" no-body>
             <div class="d-flex pb-1 border-bottom">
               <div>
-                <div class="font-weight-bold">
-                  Informasi Pembayaran
-                </div>
+                <div class="font-weight-bold">Informasi Pembayaran</div>
               </div>
             </div>
             <table class="mt-1">
               <tbody>
                 <tr>
                   <td>Bank</td>
-                  <td class="font-weight-bold">
-                    : Bank Syariah Indonesia
-                  </td>
+                  <td class="font-weight-bold">: Bank Syariah Indonesia</td>
                 </tr>
                 <tr>
                   <td>Akun</td>
-                  <td class="font-weight-bold">
-                    : IDI JAKPUS
-                  </td>
+                  <td class="font-weight-bold">: IDI JAKPUS</td>
                 </tr>
                 <tr>
                   <td>Rekening</td>
-                  <td class="font-weight-bold">
-                    : 7132822063
-                  </td>
+                  <td class="font-weight-bold">: 7132822063</td>
                 </tr>
                 <tr>
                   <td>Keterangan</td>
@@ -91,10 +98,20 @@
             </table>
           </b-card>
 
-          <b-form-group label="Upload Bukti Bayar *" label-for="upload-bukti-bayar" class="mt-1">
-            <validation-provider #default="{ errors }" name="upload-bukti-bayar" rules="required">
-              <b-form-file id="upload-bukti-bayar" :state="errors.length > 0 ? false : null" v-model="tempBuktiBayar"
-                accept="image/*" @change="buktiBayar($event)" />
+          <b-form-group
+            label="Upload Bukti Bayar *"
+            label-for="upload-bukti-bayar"
+            class="mt-1">
+            <validation-provider
+              #default="{ errors }"
+              name="upload-bukti-bayar"
+              rules="required">
+              <b-form-file
+                id="upload-bukti-bayar"
+                :state="errors.length > 0 ? false : null"
+                v-model="tempBuktiBayar"
+                accept="image/*"
+                @change="buktiBayar($event)" />
               <small class="text-danger">{{ errors[0] }}</small>
             </validation-provider>
           </b-form-group>
@@ -102,16 +119,18 @@
           <b-card class="shadow-none border p-1 mb-1" no-body>
             <div class="d-flex pb-1 border-bottom">
               <div>
-                <div class="font-weight-bold">
-                  Bukti Pembayaran
-                </div>
+                <div class="font-weight-bold">Bukti Pembayaran</div>
               </div>
             </div>
             <table class="mt-1">
               <tbody>
                 <tr v-if="invoices.data.invoice.reginvoice_file != null">
                   <td>
-                    <img :src="invoices.data.invoice.reginvoice_file" alt="gallery_image" width="320" height="280" />
+                    <img
+                      :src="invoices.data.invoice.reginvoice_file"
+                      alt="gallery_image"
+                      width="320"
+                      height="280" />
                   </td>
                 </tr>
                 <tr v-else>
@@ -121,7 +140,11 @@
             </table>
           </b-card>
 
-          <b-button type="submit" variant="outline-danger" block @click="validateUploadBuktiBayar">
+          <b-button
+            type="submit"
+            variant="outline-danger"
+            block
+            @click="validateUploadBuktiBayar">
             Simpan
           </b-button>
         </b-form>
@@ -147,13 +170,13 @@ import {
   VBModal,
   BCardText,
   BImg,
-} from 'bootstrap-vue'
-import BaseNavigation from '@/components/Base/BaseNavigation.vue'
-import DividerNavigation from '@/components/Base/DividerNavigation.vue'
-import { ValidationProvider, ValidationObserver } from 'vee-validate'
-import { required } from '@validations'
+} from "bootstrap-vue";
+import BaseNavigation from "@/components/Base/BaseNavigation.vue";
+import DividerNavigation from "@/components/Base/DividerNavigation.vue";
+import { ValidationProvider, ValidationObserver } from "vee-validate";
+import { required } from "@validations";
 
-import apis from '@/api'
+import apis from "@/api";
 
 export default {
   components: {
@@ -175,7 +198,7 @@ export default {
     BImg,
   },
   directives: {
-    'b-modal': VBModal,
+    "b-modal": VBModal,
   },
   data() {
     return {
@@ -197,7 +220,7 @@ export default {
       //   reksip_npa_file: '',
       //   reksip_alamat_instansi: '',
       // }
-    }
+    };
   },
   /**
    * computer method is not use karena bukan untuk fetch file ini hanya invoice jadi di maitiin
@@ -293,9 +316,9 @@ export default {
       // },
     },
   },
-  mounted() { },
+  mounted() {},
   created() {
-    this.fetchInvoiceCalonAnggota()
+    this.fetchInvoiceCalonAnggota();
   },
   methods: {
     // changeKompetensi(item) {
@@ -320,50 +343,73 @@ export default {
     // },
 
     validationForm() {
-      this.$refs.rekomendasiValidation.validate().then(success => {
+      this.$refs.rekomendasiValidation.validate().then((success) => {
         if (success) {
-          this.submitRekomendasi()
+          this.submitRekomendasi();
         }
-      })
+      });
     },
 
     validateUploadBuktiBayar() {
-      this.$refs.buktiBayarValidation.validate().then(success => {
+      this.$refs.buktiBayarValidation.validate().then((success) => {
         if (success) {
-          this.submitBuktiBayar()
+          this.submitBuktiBayar();
         }
-      })
+      });
     },
 
     buktiBayar(e) {
-      const { files } = e.target
+      const { files } = e.target;
       if (files.length) {
-        this.createImage(files[0], result => {
-          this.fileBerkas = result
-        })
+        this.createImage(files[0], (result) => {
+          this.fileBerkas = result;
+        });
       }
     },
 
     createImage(file, cb) {
-      const reader = new FileReader()
+      const reader = new FileReader();
 
-      reader.onload = e => {
-        cb(e.target.result)
-      }
-      reader.readAsDataURL(file)
+      reader.onload = (e) => {
+        cb(e.target.result);
+      };
+      reader.readAsDataURL(file);
     },
 
+    // async submitBuktiBayar() {
+    //   this.$store.commit("app/UPDATE_LOADING_BLOCK", true);
+    //   try {
+    //     await apis.invoice.uploadBuktiBayar(this.invoices.data.invoice.id, {
+    //       reginvoice_file: this.fileBerkas,
+    //     });
+    //     // console.log({
+    //     //   id: this.invoices.data.invoice.id,
+    //     //   data: this.fileBerkas,
+    //     // });
+    //     this.successHandler("berhasil upload bukti bayar");
+    //   } catch (error) {
+    //     this.errorHandler(error, "gagal upload bukti bayar");
+    //   } finally {
+    //     this.$store.commit("app/UPDATE_LOADING_BLOCK", false);
+    //   }
+    // },
 
-    async submitBuktiBayar() {
-      this.$store.commit('app/UPDATE_LOADING_BLOCK', true)
-      try {
-        await apis.invoice.uploadBuktiBayar(this.invoices.data.invoice.id, { reginvoice_file: this.fileBerkas })
-        this.successHandler('berhasil upload bukti bayar')
-      } catch (error) {
-        this.errorHandler(error, 'gagal upload bukti bayar')
-      } finally {
-        this.$store.commit('app/UPDATE_LOADING_BLOCK', false)
-      }
+    submitBuktiBayar() {
+      this.$store.commit("app/UPDATE_LOADING_BLOCK", true);
+      apis.invoice
+        .uploadBuktiBayar(this.invoices.data.invoice.id, {
+          reginvoice_file: this.fileBerkas,
+        })
+        .then(() => {
+          this.successHandler("berhasil upload bukti invoice");
+          location.reload();
+        })
+        .catch((error) => {
+          this.errorHandler(error, "gagal up invoice coba lagi");
+        })
+        .finally(() => {
+          this.$store.commit("app/UPDATE_LOADING_BLOCK", false);
+        });
     },
 
     // method ini buat rekomendasi umum dan spesialis jadi maiitn
@@ -401,17 +447,18 @@ export default {
     // },
 
     fetchInvoiceCalonAnggota() {
-      this.invoices.isLoading = true
-      apis.invoice.getInvoice(this.$route.params.id)
+      this.invoices.isLoading = true;
+      apis.invoice
+        .getInvoice(this.$route.params.id)
         .then(({ data }) => {
-          this.invoices.data = data
+          this.invoices.data = data;
         })
         .finally(() => {
-          this.invoices.isLoading = false
-        })
+          this.invoices.isLoading = false;
+        });
     },
   },
-}
+};
 </script>
 
 <style></style>
