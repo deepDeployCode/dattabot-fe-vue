1346c948901373f0198672eeeece52e9629036a7
devxamp <muhdevapp@gmail.com>
2023-11-03 21:01:49 +0800

    identation code

--- src/helpers/storage.js
+++ src/helpers/storage.js
@@ -1,21 +1,23 @@
-import Cookie from 'universal-cookie'
+import Cookie from "universal-cookie";
 
-const cookie = new Cookie()
+const cookie = new Cookie();
 
 const methodsOfCookie = {
   cookie,
-  getStorage: key => cookie.get(key, { path: '/' }),
+  getStorage: (key) => cookie.get(key, { path: "/" }),
   setStorage: (key, value) => {
-    cookie.set(key, value, { path: '/' })
+    cookie.set(key, value, { path: "/" });
   },
-  removeItemStorage: key => {
-    cookie.remove(key, { path: '/' })
+  removeItemStorage: (key) => {
+    cookie.remove(key, { path: "/" });
   },
   clearStorage: () => {
-    const cookieNames = Object.keys(cookie.getAll())
+    const cookieNames = Object.keys(cookie.getAll());
 
-    cookieNames.forEach(cookieName => cookie.remove(cookieName, { path: '/' }))
+    cookieNames.forEach((cookieName) =>
+      cookie.remove(cookieName, { path: "/" })
+    );
   },
-}
+};
 
-export default methodsOfCookie
+export default methodsOfCookie;
