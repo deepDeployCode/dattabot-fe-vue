d53cbd334b5dd5a677def1c10793de38a179a979
syamsulapp <samsul.marif100@gmail.com>
2023-12-12 21:41:39 +0800

    try learn chart js

---
+++ src/components/data-visualization/BaseDataVisualization.vue
@@ -0,0 +1,27 @@
+<template>
+  <!-- <Bar id="my-chart-id" :options="chartOptions" :data="chartData" /> -->
+</template>
+
+<script>
+// import { Bar } from "vue-chartjs";
+
+export default {
+  name: "data-visualization",
+  components: { Bar },
+  data() {
+    return {
+      // chartData: {
+      //   labels: ["January", "February", "March"],
+      //   datasets: [{ data: [40, 20, 12] }],
+      // },
+      // chartOptions: {
+      //   responsive: true,
+      // },
+    };
+  },
+
+  methods: {},
+  computed: {},
+  created() {},
+};
+</script>
